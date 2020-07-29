fbb7c494d692a9543847997848af41b7fd7e90d7
John Madrigal <johnmadrigal17@gmail.com>
2020-07-29 14:02:53 -0700

    added jumbotron to Nomatch for small templating style

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Jumbotron} from 'react-bootstrap'
-// import brokenHeart from '../images/brokenHeart.jpg'
-const image = 'https://i.imgur.com/iliJAZF.jpg'
+import { Jumbotron, Container } from 'react-bootstrap'
+// // import brokenHeart from '../images/brokenHeart.jpg'
+// const image = 'https://i.imgur.com/iliJAZF.jpg'
+const style = {
+  backgroundImage: `url(https://i.pinimg.com/originals/e6/c1/a9/e6c1a9c51db5983bc88100648d5f3dfb.jpg)`,
+  backgroundPosition: 'right bottom 500px',
+  color: '#f8f8ff',
+}
 
 const NoMatch = () => {
   return (
-    <Jumbotron fluid className="jumbo">
-      <h1>Looks like you wandered away from us</h1>
+    <Jumbotron fluid className="jumbo" style={style}>
+      <Container>
+      <h1>You're breaking my heart</h1>
+      <h1>Come back to SWEethearts!!</h1>
+      </Container>
       </Jumbotron>
     );
 }
